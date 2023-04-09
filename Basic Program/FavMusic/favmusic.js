8c40c37ad44b516bd0f619bec594dc6806d72702
ramsatti <ramsatti22@gmail.com>
2023-04-09 11:48:44 -0700

    My Commit

---
+++ Basic Program/FavMusic/favmusic.js
@@ -0,0 +1,18 @@
+const epicMix = [
+    "Time to Pretend",
+    "O-o-oh Child",
+    "Wish You Were Here",
+    "Heroes",
+    "I Put a Spell on You",
+    "Call Me",
+    "Paper Planes",
+    "Jolene",
+    "You Don't Own Me",
+    "Fast Car",
+    "Run the World (Girls)",
+    "Superstition"
+]
+
+for (let i = 0; i < epicMix.length; i++) {
+  console.log(epicMix[i])
+}

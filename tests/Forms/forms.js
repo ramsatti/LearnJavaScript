e08fbd8b1194a7c153ae3d3eb9a81d784d1b0884
ramsatti <ramsatti22@gmail.com>
2023-04-10 12:18:44 -0700

    My Commit

---
+++ tests/Forms/forms.js
@@ -0,0 +1,15 @@
+function data() {
+    let user = document.getElementById('name').value
+    let date = document.getElementById('dob').value
+    let gender = document.getElementById('gender').value
+    let hobbies = document.getElementById('hobbies').value
+    let beginner = document.getElementById('beginner').value
+    let intermediate = document.getElementById('intermediate').value
+    let experienced = document.getElementById('experienced').value
+    let agree = document.getElementById('agree').value
+    let label = document.getElementById('label').value
+    let submitButton = document.querySelector('input[type="submit"]').value
+
+    fullData = user, date, gender, hobbies, beginner, intermediate, experienced, agree, label, submitButton
+    alert(fullData)
+}

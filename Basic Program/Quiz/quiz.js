fabf72f64140fa0aef354ae9f16f61fcfe7f3f50
ramsatti <ramsatti22@gmail.com>
2023-04-11 21:46:14 -0700

    My Commit

--- Basic Program/Quiz/quiz.js
+++ Basic Program/Quiz/quiz.js
@@ -1,14 +1,14 @@
 let qna = [{
-    'question': 'Question 1: Who Made This Program (Hint: Youngest One in Our Family)?',
-    'ans': 1,
+    'question': '<strong><u>Question 1: Who Made This Program (Hint: Youngest One in Our Family)?</u></strong>',
+    'ansnewVal': 1,
     'o1': 'Ram',
     'o2': 'Varshith',
     'o3': 'Hema (Mom)',
     'o4': 'Siva (Dad)'
 },
 {
-    "question": "Question 2: Who Gave This Project?",
-    "ans": 1,
+    "question": "<strong><u>Question 2: Who Gave This Project?</u></strong>",
+    "ansnewVal": 1,
     "o1": "Varshith",
     "o2": "Ram",
     "o3": "Hema",
@@ -20,6 +20,13 @@ let index = 0
 let score = 0
 
 function displayQstn() {
+    if (index >= qna.length) {
+        index = 0
+        alert ('Your Final Score = ' + score + '. You Can See The Final Score Top Right of WebPage')
+        finalScore = document.getElementById('score').innerHTML = 'Final Score = ' + score
+        score = 0
+        location.reload()
+    }
     let q2d = qna[index]
     let question = document.getElementById('q')
     question.innerHTML = q2d.question
@@ -34,39 +41,63 @@ function displayQstn() {
 }
 
 function checkAns() {
-    let ans = qna[index].ans
-    console.log(ans)
+    let ansnewVal = qna[index].ansnewVal
     let op1 = document.getElementById('coption1').checked
     let op2 = document.getElementById('coption2').checked
     let op3 = document.getElementById('coption3').checked
     let op4 = document.getElementById('coption4').checked
     let printing = document.getElementById('printer')
 
-    if (ans == 1 && op1) {
+    if (ansnewVal == 1 && op1) {
         printing.innerHTML = 'You are Correct!'
         ++score
         ++index
         displayQstn()
     }
-    else if (ans == 2 && op2) {
+    else if (ansnewVal == 2 && op2) {
         printing.innerHTML = 'You are Correct'
         ++score
         ++index
         displayQstn()
     }
-    else if (ans == 3 && op3) {
+    else if (ansnewVal == 3 && op3) {
         printing.innerHTML = 'You are Correct'
         ++score
         ++index
         displayQstn()
     }
-    else if (ans == 4 && op4) {
+    else if (ansnewVal == 4 && op4) {
         printing.innerHTML = 'You are Correct'
         ++score
         ++index
+        displayQstn()
+    }
+    else {
+        print.innerHTML = 'You are Incorrect Unforunately'
+        --score
+    }
+}
+
+function modify() {
+    let pwdCheck = prompt('What is the Password? ')
+    if (pwdCheck == 'codeNerd') {
+        let questionNewValue = prompt('What is the Question? ')
+        qna[index].question = questionNewValue
+        let o1NewValue = prompt('What is Option 1?')
+        qna[index].o1 = o1NewValue
+        let o2NewValue = prompt('What is Option 2?')
+        qna[index].o2 = o2NewValue
+        let o3NewValue = prompt('What is Option 3?')
+        qna[index].o3 = o3NewValue
+        let o4NewValue = prompt('What is Option 4?')
+        qna[index].o4 = o4NewValue
+        let ansnewVal = prompt('What Option is the Answer?')
+        alert(Number(ansnewVal))
+        qna[index].ansnewVal = Number(ansnewVal)
         displayQstn()
     }
 }
+
 
 displayQstn()
 console.log(score)

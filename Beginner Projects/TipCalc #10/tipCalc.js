02bec537b51fb44550caa69f2bd9389ecf7ae959
ramsatti <ramsatti22@gmail.com>
2023-04-20 17:50:20 -0700

    My Commit

---
+++ Beginner Projects/TipCalc #10/tipCalc.js
@@ -0,0 +1,25 @@
+const amount = document.querySelector("#amount")
+const guests = document.querySelector("#guests")
+const quality = document.querySelector("#quality")
+const tipAmount = document.querySelector("#tip-amount")
+
+calculate = () => {
+    const tip = ((amount.value * quality.value) / (guests.value)).toFixed(2)
+    amount.value = ""
+    guests.value = ""
+    quality.value = ""
+    if (tip === "NaN") {
+        tipAmount.textContent = "Tip $0 each"
+        showTipAmount()
+    }
+    else {
+        tipAmount.textContent = "Tip $" + tip + " each"
+        showTipAmount()
+    }
+}
+
+showTipAmount = () => {
+    let x = document.querySelector("#tip-amount")
+    x.className = "show"
+    setTimeout(function () { x.className = x.className.replace("show", "") }, 3000)
+}
